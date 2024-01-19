93e1194d7517bc6fe25dd44abd056a3856d89be9
Parker Nilson <parker.todd.nilson@gmail.com>
2024-01-18 20:46:00 -0700

    implement hash and equals and new constructors

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -18,6 +18,18 @@ public class ChessMove {
         this.promotionPiece = promotionPiece;
     }
 
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition) {
+        this(startPosition, endPosition, null);
+    }
+
+    public ChessMove(ChessPosition startPosition, int row, int col) {
+        this(startPosition, new ChessPosition(row, col));
+    }
+
+    public ChessMove(ChessPosition startPosition, int row, int col, ChessPiece.PieceType promotionPiece) {
+        this(startPosition, new ChessPosition(row, col), promotionPiece);
+    }
+
     /**
      * @return ChessPosition of starting location
      */
@@ -40,5 +52,40 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((endPosition == null) ? 0 : endPosition.hashCode());
+        result = prime * result + ((promotionPiece == null) ? 0 : promotionPiece.hashCode());
+        result = prime * result + ((startPosition == null) ? 0 : startPosition.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ChessMove other = (ChessMove) obj;
+        if (endPosition == null) {
+            if (other.endPosition != null)
+                return false;
+        } else if (!endPosition.equals(other.endPosition))
+            return false;
+        if (promotionPiece != other.promotionPiece)
+            return false;
+        if (startPosition == null) {
+            if (other.startPosition != null)
+                return false;
+        } else if (!startPosition.equals(other.startPosition))
+            return false;
+        return true;
     }
 }
