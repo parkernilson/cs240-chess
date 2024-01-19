e7e8981713d05c8b720a075badff5bc5de0c9cac
Parker Nilson <parker.todd.nilson@gmail.com>
2024-01-18 20:46:17 -0700

    implement hash and equals

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -10,12 +11,26 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    ChessGame.TeamColor pieceColor;
-    ChessPiece.PieceType type;
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
+    private final int FORWARD;
+    private final int BACKWARD;
+    private final int RIGHT;
+    private final int LEFT;
+
+    static int BLACK_PAWN_HOME_ROW = 7;
+    static int WHITE_PAWN_HOME_ROW = 2;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+        // if black, then forward is -1, if white, then forward is +1 (and backward is
+        // opposite)
+        FORWARD = pieceColor == ChessGame.TeamColor.BLACK ? -1 : 1;
+        BACKWARD = -FORWARD;
+        // if black, then right is +1, if white, then right is -1 (and left is opposite)
+        RIGHT = pieceColor == ChessGame.TeamColor.BLACK ? -1 : 1;
+        LEFT = -RIGHT;
     }
 
     /**
@@ -28,36 +43,6 @@ public class ChessPiece {
         KNIGHT,
         ROOK,
         PAWN
-    }
-
-    private static Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
-    }
-
-    private static Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
-    }
-
-    private static Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
-    }
-
-    private static Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
-    }
-
-    private static Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
-    }
-
-    private static Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
-
-        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -82,23 +67,51 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
         // based on which type this piece is, call the appropriate helper method
         // return the result of the helper method
         switch (type) {
             case PAWN:
-                return pawnMoves(board, myPosition);
+                PawnMovesCalculator pawnCalc = new PawnMovesCalculator(pieceColor);
+                return pawnCalc.pieceMoves(board, myPosition);
             case ROOK:
-                return rookMoves(board, myPosition);
+                RookMovesCalculator rookCalc = new RookMovesCalculator(pieceColor);
+                return rookCalc.pieceMoves(board, myPosition);
             case KNIGHT:
-                return knightMoves(board, myPosition);
+                KnightMovesCalculator knightCalc = new KnightMovesCalculator(pieceColor);
+                return knightCalc.pieceMoves(board, myPosition);
             case BISHOP:
-                return bishopMoves(board, myPosition);
+                BishopMovesCalculator bishopCalc = new BishopMovesCalculator(pieceColor);
+                return bishopCalc.pieceMoves(board, myPosition);
             case QUEEN:
-                return queenMoves(board, myPosition);
+                throw new RuntimeException("Not implemented");
             case KING:
-                return kingMoves(board, myPosition);
+                throw new RuntimeException("Not implemented");
             default:
                 throw new RuntimeException(String.format("Unsupported piece type: %s", type));
         }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((pieceColor == null) ? 0 : pieceColor.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        ChessPiece other = (ChessPiece) obj;
+        if (pieceColor != other.pieceColor)
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
     }
 }
