36b9dfbfed0b6eaa71dd2f5cdf2dd2f9b570cc8f
Parker Nilson <parker.todd.nilson@gmail.com>
2024-03-05 10:52:08 -0700

    Set up skeleton for sql in game dao

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -1,13 +1,34 @@
 package dataAccess;
 
-import java.sql.SQLException;
-
 import model.GameData;
 
-public class SQLGameDAO implements GameDAO {
+public class SQLGameDAO extends SQLDAO implements GameDAO {
+
+    static private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                `game_id` varchar(256) NOT NULL,
+                `white_username` varchar(256) DEFAULT NULL,
+                `black_username` varchar(256) DEFAULT NULL,
+                `game_name` varchar(256) NOT NULL,
+                `game_state` varchar(256) NOT NULL,
+                PRIMARY KEY (`game_id`),
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS participants (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `game_id` varchar(256) NOT NULL,
+                `username` varchar(256) NOT NULL,
+                PRIMARY KEY (`id`),
+                INDEX(username)
+                INDEX(game_id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
 
     SQLGameDAO() throws ResponseException, DataAccessException {
-        configureDatabase();
+        configureDatabase(createStatements);
     }
 
     public GameData getGame(int gameId) {
@@ -37,41 +58,5 @@ public class SQLGameDAO implements GameDAO {
     public int getMaxGameId() {
         throw new UnsupportedOperationException("Not implemented yet");
     }
-
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS games (
-              `game_id` varchar(256) NOT NULL,
-              `white_username` varchar(256) DEFAULT NULL,
-              `black_username` varchar(256) DEFAULT NULL,
-              `game_name` varchar(256) NOT NULL,
-              `game_state` varchar(256) NOT NULL,
-              PRIMARY KEY (`game_id`),
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """,
-            """
-            CREATE TABLE IF NOT EXISTS participants (
-              `id` int NOT NULL AUTO_INCREMENT,
-              `game_id` varchar(256) NOT NULL,
-              `username` varchar(256) NOT NULL,
-              PRIMARY KEY (`id`),
-              INDEX(username)
-              INDEX(game_id)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
-
-    private void configureDatabase() throws ResponseException, DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex) {
-            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-    }
-
 
 }
