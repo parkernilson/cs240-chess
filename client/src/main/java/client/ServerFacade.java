5683772059fb560940696221fffd3d0e5de9e8bb
Parker Nilson <parker.todd.nilson@gmail.com>
2024-03-13 15:34:28 -0600

    stub out some methods

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -7,4 +7,32 @@ public class ServerFacade {
     public ServerFacade(String serverUrl) {
         this.serverUrl = serverUrl;
     }
+
+    // TODO: wherever response data should be read, either return a
+    // record type from the shared module, or create a response record type
+    // Also, wherever useful take one of the record types as input
+
+    public void registerUser(String name, String password) {
+
+    }
+
+    public void signIn(String name, String password) {
+
+    }
+
+    public void signOut() {
+
+    }
+
+    public void listGames() {
+        
+    }
+
+    public void createGame() {
+
+    }
+
+    public void joinGame(int gameId) {
+
+    }
 }
