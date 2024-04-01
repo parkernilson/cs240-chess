ef10c07f885d14e380dd4fa3f9704479dea80e27
Parker Nilson <parker.todd.nilson@gmail.com>
2024-04-01 13:09:05 -0600

    Set up example web socket handler

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,55 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import exceptions.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.serverMessages.ServerMessage.ServerMessageType;
+import webSocketMessages.userCommands.UserGameCommand.CommandType;
+import webSocketMessages.userCommands.UserGameCommand;
+
+import java.io.IOException;
+import java.util.Timer;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
+        switch (action.getCommandType()) {
+            case CommandType.JOIN_PLAYER -> joinPlayer(action.getAuthString(), session);
+            case CommandType.JOIN_OBSERVER -> joinObserver();
+            case CommandType.MAKE_MOVE -> makeMove();
+            case CommandType.LEAVE -> leave();
+            case CommandType.RESIGN -> resign();
+        }
+    }
+
+    private void joinPlayer(String authToken, Session session) throws IOException {
+        connections.add(authToken, session);
+        var notification = new ServerMessage(ServerMessageType.NOTIFICATION);
+        connections.broadcast(authToken, notification);
+    }
+
+    private void joinObserver() {
+
+    }
+
+    private void makeMove() {
+
+    }
+
+    private void leave() {
+
+    }
+
+    private void resign() {
+
+    }
+}
